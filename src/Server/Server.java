91ce20e541c96587728f9dfa691ecb0edc0ad131
SergioQuintanilla <sergioquintanillagracia@gmail.com>
2024-11-20 12:36:15 +0100

    Started working on server code

---
+++ src/Server/Server.java
@@ -0,0 +1,85 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+class Server {
+    static final String VERSION = "0.1";
+    private int port;
+    private LinkedList<User> users = new LinkedList<>();
+
+    public Server(int port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) {
+        int port = 49200;
+        Server server = new Server(port);
+        server.start();
+    }
+
+    public void start() {
+        try (ServerSocket server = new ServerSocket(port)) {
+            System.out.printf("Server version %s is now listening on port %d\n", VERSION, port);
+
+            while (true) {
+                // Accept an incoming connection
+                Socket client = server.accept();
+
+                try {
+                    // Create a user object for the connection
+                    User user = new User(client);
+
+                    // Start a thread to handle the connection
+                    ClientThread ct = new ClientThread(this, user);
+                    ct.start();
+
+                } catch (IOException e) {
+                    // Handle IOExceptions that occur when creating a user
+                    System.err.println(e.getMessage());
+                    client.close();
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.printf("I/O error occurred at server opened at port %d\n", port);
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    /* Sends a message to a specific user */
+    public void sendMessage(String msg, User fromUser, User toUser) {
+        toUser.sendMessage(msg, fromUser);
+    }
+
+    /* Sends a message to all users connected to the server */
+    public void broadcastMessage(String msg, User fromUser) {
+        for (User u : users) {
+            // Send the message to every user except the sender
+            if (u != fromUser) {
+                sendMessage(msg, fromUser, u);
+            }
+        }
+    }
+}
+
+
+class ClientThread extends Thread {
+    User user;
+    Server server;
+
+    public ClientThread(Server server, User user) {
+        this.server = server;
+        this.user = user;
+    }
+
+    @Override
+    public void run() {
+        while (user.hasNextMessage()) {
+            server.broadcastMessage(user.getNextMessage(), user);
+        }
+    }
+}
---
+++ src/Server/User.java
@@ -0,0 +1,55 @@
+package Server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+class User {
+    // User ID, incremented by 1 for each created user
+    static int uid = 0;
+
+    Socket socket;
+    Scanner input;
+    PrintWriter output;
+
+    public User(Socket s) throws IOException {
+        socket = s;
+
+        // Create input and output streams
+        try {
+            input = new Scanner(s.getInputStream());
+            output = new PrintWriter(s.getOutputStream());
+
+        } catch (IOException e) {
+            // Throw an IOException with a custom message to inform there was an error creating the user
+            throw new IOException("Couldn't get input / output stream of user socket");
+        }
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public String getName() {
+        // TODO: Implement username system, at the moment users will be identified by their UID, which changes
+        // TODO: every time they reconnect (which is not ideal)
+        return Integer.toString(uid);
+    }
+
+    public boolean hasNextMessage() {
+        return input.hasNextLine();
+    }
+
+    public String getNextMessage() {
+        return input.nextLine();
+    }
+
+    public void sendMessage(String msg, User fromUser) {
+        sendString("[%s]: %s".formatted(fromUser.getName(), msg));
+    }
+
+    private void sendString(String msg) {
+        output.printf("%s\r\n", msg);
+    }
+}
